fc7be066505bcf276b279359791dfcf0cb8a3c6e
Théo <theo.nardin@cri-paris.org>
2025-05-15 15:42:14 +0200

    add refinedoc

--- welearn_datastack/modules/pdf_extractor.py
+++ welearn_datastack/modules/pdf_extractor.py
@@ -4,108 +4,9 @@ from statistics import mean
 from typing import List, Tuple
 
 from pypdf import PdfReader
+from refinedoc.refined_document import RefinedDocument
 
 logger = logging.getLogger(__name__)
-
-
-def compare(a: str, b: str):
-    map_trans = str.maketrans("0123456789", "@" * 10)
-    a = a.translate(map_trans).lower()
-    b = b.translate(map_trans).lower()
-
-    s = SequenceMatcher(None, a, b)
-    ret = s.ratio()
-    return ret
-
-
-def compare_candidates(to_compare_candidates: list[str], from_compare: str) -> float:
-    computed = [
-        compare(from_compare, to_compare_candidate)
-        for to_compare_candidate in to_compare_candidates
-    ]
-    ret = mean(computed)
-    return ret
-
-
-def remove_header(pages: list[list[str]], header_candidates: list[list[str]], win: int):
-    """Remove headers from content dictionary. Helper function for remove_header_footer() function."""
-
-    header_weights = [1.0, 0.75, 0.5, 0.5, 0.5]
-
-    for page_index, candidates in enumerate(header_candidates):
-        local_neighbours = header_candidates[
-            max(page_index - win, 1) : min(page_index + win, len(header_candidates))
-        ]
-
-        unify_list_len(local_neighbours)
-
-        detected = detect_similar_lines(
-            candidates=candidates,
-            positional_weights=header_weights,
-            local_neighbours=local_neighbours,
-        )
-
-        pages[page_index] = [x for x in pages[page_index] if x not in detected]
-
-    return pages
-
-
-def detect_similar_lines(
-    candidates: list[str],
-    positional_weights: list[float],
-    local_neighbours: list[list[str]],
-) -> list[str]:
-    detected = []
-    for line_index, candidate in enumerate(candidates):
-        try:
-            to_compare_candidates = [x[line_index] for x in local_neighbours]
-            score = (
-                compare_candidates(
-                    to_compare_candidates=to_compare_candidates,
-                    from_compare=candidate,
-                )
-                * positional_weights[line_index]
-            )
-        except IndexError as e:
-            print(e)
-            score = positional_weights[line_index]
-        if score >= 0.5:
-            detected.append(candidate)
-    return detected
-
-
-def unify_list_len(local_neighbours, at_top: bool = False):
-    maxlen = len(max(local_neighbours, key=len))
-
-    for sublist in local_neighbours:
-        place_holders = [""] * (maxlen - len(sublist))
-        if at_top:
-            for place_holder in place_holders:
-                sublist.insert(0, place_holder)
-        else:
-            sublist.extend([""] * (maxlen - len(sublist)))
-
-
-def remove_footer(pages: list[list[str]], footer_candidates: list[list[str]], win: int):
-    """Remove footers from content dictionary. Helper function for remove_header_footer() function."""
-
-    footer_weights = [0.5, 0.5, 0.5, 0.75, 1.0]
-
-    for page_index, candidates in enumerate(footer_candidates):
-        local_neighbours = footer_candidates[
-            max(page_index - win, 1) : min(page_index + win, len(footer_candidates))
-        ]
-        unify_list_len(local_neighbours, at_top=True)
-
-        detected = detect_similar_lines(
-            candidates=candidates,
-            positional_weights=footer_weights,
-            local_neighbours=local_neighbours,
-        )
-
-        pages[page_index] = [x for x in pages[page_index] if x not in detected]
-
-    return pages
 
 
 def large_pages_size_flag(reader: PdfReader, limit: int) -> Tuple[List[int], bool]:
@@ -146,22 +47,9 @@ def extract_txt_from_pdf(
         page_content = [t.strip() for t in text if t.strip()]
         pdf_content.append(page_content)
 
-    header_candidates = []
-    footer_candidates = []
+    refined_pdf_content = RefinedDocument(content=pdf_content)
 
-    for page_content in pdf_content:
-        header_candidates.append(page_content[:5])
-        footer_candidates.append(page_content[-5:])
-
-    win = 8
-
-    if remove_headers:
-        pdf_content = remove_header(pdf_content, header_candidates, win)
-
-    if remove_footers:
-        pdf_content = remove_footer(pdf_content, footer_candidates, win)
-
-    return pdf_content
+    return refined_pdf_content.body
 
 
 def delete_non_printable_character(text: str) -> str:
