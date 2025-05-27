7ad13d9f2c67af9e27b9c70dac5fe7cc9388ae23
Jean-Marc SEVIN <jean-marc.sevin@cri-paris.org>
2025-05-27 14:29:20 +0200

    Refactor word tokenizer

--- welearn_datastack/modules/keywords_extractor.py
+++ welearn_datastack/modules/keywords_extractor.py
@@ -3,8 +3,6 @@ from typing import List
 
 from keybert import KeyBERT  # type: ignore
 from sentence_transformers import SentenceTransformer  # type: ignore
-from spacy.lang.en import English
-from spacy.lang.fr import French
 
 from welearn_datastack.data.db_models import WeLearnDocument
 from welearn_datastack.data.enumerations import MLModelsType
@@ -12,23 +10,12 @@ from welearn_datastack.modules.embedding_model_helpers import (
     get_document_embedding_model_name_from_lang,
     load_embedding_model,
 )
+from welearn_datastack.utils_.nlp_utils import tokenize
 from welearn_datastack.utils_.path_utils import generate_ml_models_path
 
 logger = logging.getLogger(__name__)
 
 loaded_models: dict[str, SentenceTransformer] = {}
-
-en_nlp = English()
-fr_nlp = French()
-
-
-def _get_nlp_model(language: str):
-    if language == "en":
-        return en_nlp
-    elif language == "fr":
-        return fr_nlp
-    else:
-        raise ValueError(f"Unsupported language: {language}")
 
 
 def extract_keywords(document: WeLearnDocument) -> List[str]:
@@ -43,12 +30,7 @@ def extract_keywords(document: WeLearnDocument) -> List[str]:
     embedding_model = load_embedding_model(ml_path.as_posix())
     kw_model = KeyBERT(model=embedding_model)
 
-    nlp_model = _get_nlp_model(str(document.lang))
-
-    doc = nlp_model(str(document.description))
-    clean_description = " ".join(
-        [token.text for token in [tk for tk in doc if not tk.is_stop]]
-    )
+    clean_description = " ".join(tokenize(str(document.description), document.lang))
     keywords = kw_model.extract_keywords(
         clean_description,
         keyphrase_ngram_range=(1, 2),
