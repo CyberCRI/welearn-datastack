b1b6465e6e73c112b1c2d37553fd0794d24a79f5
Théo <theo.nardin@cri-paris.org>
2025-06-12 16:51:51 +0200

    rm br

--- welearn_datastack/constants.py
+++ welearn_datastack/constants.py
@@ -156,7 +156,7 @@ AUTHORIZED_LICENSES = [
 HEADERS = {
     "Accept": "text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "TE": "Trailers",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/136.0",
