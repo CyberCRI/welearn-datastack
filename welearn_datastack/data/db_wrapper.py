e69e1a00950700c1deefd827dff03507fe60096f
Théo <theo.nardin@cri-paris.org>
2025-10-17 16:15:04 +0200

    use of the wrapper and remove lot of useless lines

--- welearn_datastack/data/db_wrapper.py
+++ welearn_datastack/data/db_wrapper.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from welearn_database.data.models import WeLearnDocument
+from welearn_database.data.models import ErrorRetrieval, WeLearnDocument
 
 
 class Wrapper(ABC):
@@ -8,6 +8,23 @@ class Wrapper(ABC):
 
 
 class WrapperRetrieveDocument(Wrapper):
-    def __init__(self, document: WeLearnDocument, is_retrieved: bool):
+    def __init__(
+        self,
+        document: WeLearnDocument,
+        http_error_code: int | None = None,
+        error_info: str | None = None,
+    ):
         self.document = document
-        self.is_retrieved = is_retrieved
+        self.http_error_code = http_error_code
+        self.error_info = error_info
+
+    @property
+    def is_error(self) -> bool:
+        return self.http_error_code is not None or self.error_info is not None
+
+    def to_error_retrieval(self) -> ErrorRetrieval:
+        return ErrorRetrieval(
+            document_id=self.document.id,
+            http_error_code=self.http_error_code,
+            error_info=self.error_info,
+        )
