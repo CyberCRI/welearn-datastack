32ab5a5267b0399b684a3508416935d0bef59268
Théo <theo.nardin@cri-paris.org>
2025-11-25 15:43:15 +0100

    feat: implement UVED data models and URL collector for resource extraction

---
+++ welearn_datastack/data/source_models/uved.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Category(BaseModel):
+    title: str
+    parent: Optional[Category]
+    uid: int
+    atat_id: str = Field(..., alias="@id")
+
+
+class Type(BaseModel):
+    title: str
+    parent: Optional[Type]
+    uid: int
+    at_id: str = Field(..., alias="@id")
+
+
+class MainInstitution(BaseModel):
+    name: str
+    title: str
+    uid: int
+    at_id: str = Field(..., alias="@id")
+
+
+class Keyword(BaseModel):
+    title: str
+    dewey: str
+    uid: int
+    at_id: str = Field(..., alias="@id")
+
+
+class UVEDMemberItem(BaseModel):
+    categories: list[Category]
+    type: Type
+    title: str
+    url: str
+    date: str
+    duration: int
+    description: str
+    contexte: Optional[str]
+    slug: str
+    rate: int
+    star: int
+    mainInstitution: MainInstitution
+    keywords: list[Keyword]
+    uid: int
+    at_id: str = Field(..., alias="@id")
+
+
+class HydraView(BaseModel):
+    hydra_first: str = Field(..., alias="hydra:first")
+    hydra_last: str = Field(..., alias="hydra:last")
+    hydra_next: Optional[str] = Field(default=None, alias="hydra:next")
+    hydra_prev: Optional[str] = Field(default=None, alias="hydra:prev")
+    hydra_pages: list[str] = Field(..., alias="hydra:pages")
+    hydra_page: int = Field(..., alias="hydra:page")
+
+
+class HydraMappingItem(BaseModel):
+    variable: str
+    property: str
+
+
+class HydraSearch(BaseModel):
+    hydra_template: str = Field(..., alias="hydra:template")
+    hydra_mapping: list[HydraMappingItem] = Field(..., alias="hydra:mapping")
+
+
+class RootUVEDModel(BaseModel):
+    hydra_member: list[UVEDMemberItem] = Field(..., alias="hydra:member")
+    hydra_totalItems: int = Field(..., alias="hydra:totalItems")
+    hydra_view: HydraView = Field(..., alias="hydra:view")
+    hydra_search: HydraSearch = Field(..., alias="hydra:search")
