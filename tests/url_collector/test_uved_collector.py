42d1c4d5973b974d80c0e11ce8a3f4276c486080
Théo <theo.nardin@cri-paris.org>
2025-11-25 10:51:00 +0100

    chore: update welearn-database version to 1.0.0.dev1 and add tests for UVEDURLCollector

---
+++ tests/url_collector/test_uved_collector.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+from unittest import TestCase
+from unittest.mock import patch
+
+from welearn_database.data.models import Corpus
+
+from welearn_datastack.collectors.uved_collector import UVEDURLCollector
+
+
+class Test(TestCase):
+    def setUp(self) -> None:
+        self.mock_corpus = Corpus(source_name="uved", is_fix=True)
+
+        self.ted_file_path = Path(__file__).parent / "resources" / "ted_file.json"
+        with self.ted_file_path.open(mode="r") as f:
+            self.ted_content = f.read()
+
+    @patch("welearn_datastack.collectors.ted_collector.requests.post")
+    def test_ted_urlcollector(self, mock_post):
+        """
+        Test the collect method of the TedURLCollector class, on a json file with 3 rows
+        """
+
+        mock_post.return_value.ok = True
+        mock_post.return_value.json.return_value = json.loads(self.ted_content)
+
+        ted_collector = UVEDURLCollector(
+            corpus=self.mock_corpus,
+        )
+        collected = ted_collector.collect()
+        self.assertEqual(3, len(collected))
+
+        for i in range(0, len(collected)):
+            self.assertEqual(
+                collected[i].url, f"https://www.ted.com/talks/ted_talk_{i+1}"
+            )
+            self.assertEqual(collected[i].corpus.source_name, "ted")
+            self.assertEqual(collected[i].corpus.is_fix, True)
