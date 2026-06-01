c98791c0297a14510a58ee107b0e5fb6221b79fe
Théo <theo.nardin@cri-paris.org>
2026-06-01 16:32:10 +0200

    feat: refactor document retrieval and insertion logic to improve error handling and processing

--- welearn_datastack/nodes_workflow/DocumentHubCollector/document_collector.py
+++ welearn_datastack/nodes_workflow/DocumentHubCollector/document_collector.py
@@ -2,15 +2,16 @@ import csv
 import logging
 import os
 import uuid
-from dataclasses import asdict
-from typing import Dict, List, Tuple
+from typing import Dict, List
 from uuid import UUID
 
+from sqlalchemy import exc
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.attributes import flag_modified
 from welearn_database.data.enumeration import Step
-from welearn_database.data.models import ErrorRetrieval, ProcessState, WeLearnDocument
+from welearn_database.data.models import ProcessState, WeLearnDocument
 
+from welearn_datastack.data.db_wrapper import WrapperRetrieveDocument
 from welearn_datastack.exceptions import PluginNotFoundError
 from welearn_datastack.modules import collector_selector
 from welearn_datastack.modules.computed_metadata import (
@@ -19,7 +20,6 @@ from welearn_datastack.modules.computed_metadata import (
     identify_document_language,
     serialize_dataclass_instance,
 )
-from welearn_datastack.modules.validation import validate_non_null_fields_document
 from welearn_datastack.plugins.interface import IPlugin
 from welearn_datastack.utils_.database_utils import create_db_session
 from welearn_datastack.utils_.path_utils import setup_local_path
@@ -45,7 +45,7 @@ def main() -> None:
     input_artifact_id_url = os.getenv("ARTIFACT_ID_URL_CSV_NAME", "batch_ids.csv")
     logger.info("Input artifact url csv name: %s", input_artifact_id_url)
 
-    local_artifcat_input, local_artifcat_output = setup_local_path()
+    local_artifcat_input, _ = setup_local_path()
 
     # retrieve url data from files
     logger.info("Retrieve URLs from file")
@@ -82,31 +82,98 @@ def main() -> None:
 
     # Data extraction
     logger.info("Data extraction - Retrieve URLs and documents")
-    batch_documents, errors, states = extract_data_from_urls(welearn_documents)
+    all_documents = extract_data_from_urls(welearn_documents)
+
+    # Filter
+    correct_docs = [d for d in all_documents if not d.is_error]
+    errors = [d for d in all_documents if d.is_error]
+    logger.info("'%s' documents were correctly retrieved", len(correct_docs))
+    logger.info("'%s' documents were in error during retrievement", len(errors))
+
+    #  Data insertion
+    insertion_error = _insert_documents(db_session, correct_docs)
+    errors.extend([e for e in insertion_error if e not in errors])
+    _insert_errors(db_session, errors)
+
+    logger.info("Data insertion - Documents and errors were inserted in database")
+
+    db_session.commit()
+    db_session.close()
+    logger.info("DocumentCollectorHub finished")
+
+
+def _insert_documents(
+    db_session, docs: list[WrapperRetrieveDocument]
+) -> list[WrapperRetrieveDocument]:
+    """
+    Insert documents in database and their states
+    :db_session: Database session
+    :docs: Not in error documents to insert
+    :return: List of documents in error during insertion
+    """
+    ret_errors: list[WrapperRetrieveDocument] = []
+    duplicates_errors: list[WrapperRetrieveDocument] = []
+    retrievement_errors: list[WrapperRetrieveDocument] = []
+
+    for wrapper_retrieve_document in docs:
+        if wrapper_retrieve_document.is_error:
+            retrievement_errors.append(wrapper_retrieve_document)
+            continue
+
+        if not wrapper_retrieve_document.document.details:
+            wrapper_retrieve_document.document.details = {}
+        identify_document_language(wrapper_retrieve_document.document)
+        compute_duration(wrapper_retrieve_document.document)
+        compute_readability(wrapper_retrieve_document.document)
+        serialize_dataclass_instance(wrapper_retrieve_document.document)
+        flag_modified(wrapper_retrieve_document.document, "details")
+        try:
+            with db_session.begin_nested():
+                db_session.add(wrapper_retrieve_document.document)
+                db_session.add(
+                    ProcessState(
+                        document_id=wrapper_retrieve_document.document.id,
+                        title=Step.DOCUMENT_SCRAPED.value,
+                    )
+                )
+        except exc.IntegrityError as e:
+            logger.warning(
+                "This document %s is a duplication",
+                wrapper_retrieve_document.document.id,
+                exc_info=e,
+            )
+            wrapper_retrieve_document.error_info = str(e)
+            duplicates_errors.append(wrapper_retrieve_document)
     logger.info("Data extraction - URLs and documents were retrieved")
 
-    # Compute some metadata
-    logger.info("Compute some metadata for retrieved documents")
-    for doc in batch_documents:
-        if not doc.details:
-            doc.details = {}
-        identify_document_language(doc)
-        compute_duration(doc)
-        compute_readability(doc)
-        serialize_dataclass_instance(doc)
-        flag_modified(doc, "details")
+    ret_errors.extend(duplicates_errors)
+    ret_errors.extend(retrievement_errors)
+    logger.info("There is %s documents in error after insertion", len(ret_errors))
+    return ret_errors
 
-    db_session.add_all(states)
-    db_session.add_all(errors)
-    db_session.add_all(batch_documents)
-    db_session.commit()
+
+def _insert_errors(db_session, errors: list[WrapperRetrieveDocument]) -> None:
+    """
+    Insert errors in database and insert the related state
+    :db_session: database session
+    :errors: list of errors
+    """
+    for wrapper_retrieve_document in errors:
+        db_session.add(wrapper_retrieve_document.to_error_retrieval())
+        db_session.add(
+            ProcessState(
+                document_id=wrapper_retrieve_document.document.id,
+                title=Step.DOCUMENT_IS_IRRETRIEVABLE.value,
+            )
+        )
+    logger.info("%s Errors were inserted in database", len(errors))
 
 
 def extract_data_from_urls(
-    welearn_documents: List[WeLearnDocument],
-) -> Tuple[List[WeLearnDocument], List[ErrorRetrieval], List[ProcessState]]:
+    welearn_documents: list[WeLearnDocument],
+) -> list[WrapperRetrieveDocument]:
     """
-    Extract_data_from_urls
+    Extract data from URLs by using the correct plugin for this related source
     :param welearn_documents: input docs
     :return: URLs and documents retrieved from DB and web
     """
@@ -127,6 +194,24 @@ def extract_data_from_urls(
             )
 
     # Select appropriate plugins
+    corpus_plugin = _select_appropriate_plugin(batch_docs)
+
+    # Retrieve documents
+    logger.info("Retrieve documents")
+    # Iterate on each corpus
+    ret_docs: list[WrapperRetrieveDocument] = []
+    for corpus_name in batch_docs:
+        # Get data
+        corpus_collector = corpus_plugin[corpus_name]
+        documents = corpus_collector.run(documents=batch_docs[corpus_name])  # type: ignore
+        ret_docs.extend(documents)
+        logger.info(
+            f"'{len(documents)}/{len(welearn_documents)}' documents were processed"
+        )
+    return ret_docs
+
+
+def _select_appropriate_plugin(batch_docs: dict[str, list]) -> dict[str, IPlugin]:
     logger.info("Select appropriate plugin")
     corpus_plugin: Dict[str, IPlugin] = {}
     error_corpus_name: List[str] = []
@@ -135,7 +220,7 @@ def extract_data_from_urls(
             current_plugin = collector_selector.select_collector(corpus=corpus_name)
         except PluginNotFoundError as e:
             logger.exception(e)
-            logger.warning("This corpus '%s" " will be ignored", corpus_name)
+            logger.warning("This corpus '%s  will be ignored", corpus_name)
             error_corpus_name.append(corpus_name)
             continue
         corpus_plugin[corpus_name] = current_plugin
@@ -146,50 +231,7 @@ def extract_data_from_urls(
         )
     for corpus_name in error_corpus_name:
         del batch_docs[corpus_name]
-
-    # Retrieve documents
-    logger.info("Retrieve documents")
-    ret_documents: List[WeLearnDocument] = []
-    error_docs: List[ErrorRetrieval] = []
-    states: List[ProcessState] = []
-
-    # Iterate on each corpus
-    for corpus_name in batch_docs:
-        # Get data
-        corpus_collector = corpus_plugin[corpus_name]
-        documents = corpus_collector.run(documents=batch_docs[corpus_name])  # type: ignore
-
-        for wrapper_document in documents:
-            is_none_valid = validate_non_null_fields_document(wrapper_document.document)
-            if not is_none_valid and not wrapper_document.is_error:
-                wrapper_document.http_error_code = 422
-                wrapper_document.error_info = (
-                    "Mandatory fields are missing after extraction"
-                )
-
-            state_title = (
-                Step.DOCUMENT_SCRAPED.value
-                if not wrapper_document.is_error
-                else Step.DOCUMENT_IS_IRRETRIEVABLE.value
-            )
-            if not wrapper_document.is_error:
-                ret_documents.append(wrapper_document.document)
-            else:
-                error_docs.append(wrapper_document.to_error_retrieval())
-            states.append(
-                ProcessState(
-                    id=uuid.uuid4(),
-                    document_id=wrapper_document.document.id,
-                    title=state_title,
-                )
-            )
-        logger.info(
-            f"'{len(ret_documents)}/{len(welearn_documents)}' documents were retrieved"
-        )
-        logger.info(
-            f"'{len(error_docs)}/{len(welearn_documents)}' errors were retrieved"
-        )
-    return ret_documents, error_docs, states
+    return corpus_plugin
 
 
 if __name__ == "__main__":
