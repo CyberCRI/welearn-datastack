56ae9b3a9476d7071cb9d9d02c9979ae34c18436
Théo <theo.nardin@cri-paris.org>
2026-06-18 17:04:31 +0200

    fix: update welearn-database version to 1.5.1 and remove unused dataclass serialization import

--- welearn_datastack/nodes_workflow/DocumentHubCollector/document_collector.py
+++ welearn_datastack/nodes_workflow/DocumentHubCollector/document_collector.py
@@ -2,7 +2,6 @@ import csv
 import logging
 import os
 import uuid
-from dataclasses import asdict
 from typing import Dict, List, Tuple
 from uuid import UUID
 
@@ -17,7 +16,6 @@ from welearn_datastack.modules.computed_metadata import (
     compute_duration,
     compute_readability,
     identify_document_language,
-    serialize_dataclass_instance,
 )
 from welearn_datastack.modules.validation import validate_non_null_fields_document
 from welearn_datastack.plugins.interface import IPlugin
@@ -134,7 +132,7 @@ def extract_data_from_urls(
             current_plugin = collector_selector.select_collector(corpus=corpus_name)
         except PluginNotFoundError as e:
             logger.exception(e)
-            logger.warning("This corpus '%s" " will be ignored", corpus_name)
+            logger.warning("This corpus '%s' will be ignored", corpus_name)
             error_corpus_name.append(corpus_name)
             continue
         corpus_plugin[corpus_name] = current_plugin
